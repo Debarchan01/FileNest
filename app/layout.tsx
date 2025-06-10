0089b028a2e289dca35411cd19d6ed45b3ccd12c
Debarchan Parida <debarchan132002@gmail.com>
2025-06-10 18:05:13 +0530

    Changes to title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Droply",
+  title: "FileNest",
   description: "Secure cloud storage for your images, powered by ImageKit",
 };
 
